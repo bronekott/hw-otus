d19dbc93a2e8b7db55f1f06b7e7997543e594af4
bronekott <bronekott@ya.ru>
2022-12-18 14:47:27 +0300

    Revert "HW3 is completed"
    This reverts commit 9b0b70bca62d4f5c3221b40228bc7df6df5b7e63.

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,39 +1,6 @@
 package hw03frequencyanalysis
 
-import (
-	"regexp"
-	"sort"
-	"strings"
-)
-
-func Top10(str string) []string {
-	var pattern = regexp.MustCompile(`(\p{L})(\p{P}$)`)
-	wordMap := make(map[string]int)
-	var wordSlice []string
-
-	for _, v := range strings.Fields(str) {
-		word := pattern.ReplaceAllString(strings.ToLower(v), "$1")
-		if word == "-" {
-			continue
-		}
-		if value, ok := wordMap[word]; ok {
-			wordMap[word] = value + 1
-		} else {
-			wordMap[word] = 1
-			wordSlice = append(wordSlice, word)
-		}
-	}
-
-	sort.Slice(wordSlice, func(i, j int) bool {
-		if wordMap[wordSlice[i]] == wordMap[wordSlice[j]] {
-			return wordSlice[i] < wordSlice[j]
-		}
-		return wordMap[wordSlice[i]] > wordMap[wordSlice[j]]
-	})
-
-	if len(wordSlice) > 10 {
-		return wordSlice[:10]
-	} else {
-		return wordSlice
-	}
+func Top10(_ string) []string {
+	// Place your code here.
+	return nil
 }
